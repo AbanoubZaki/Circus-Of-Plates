46560829cde33e61b7320bd4a48e2755e632d9d4
Abanoub Ashraf <Abanoub Ashraf@DESKTOP-17KSTCF>
2018-12-16 21:30:54 +0200

    1

---
+++ src/eg/edu/alexu/csd/oop/game/cs01/objects/FallenObject.java
@@ -0,0 +1,57 @@
+package eg.edu.alexu.csd.oop.game.cs01.objects;
+
+import java.awt.image.BufferedImage;
+
+import eg.edu.alexu.csd.oop.game.GameObject;
+
+public class FallenObject implements GameObject{
+
+	@Override
+	public int getX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setX(int x) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setY(int y) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isVisible() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public BufferedImage[] getSpriteImages() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
