b5aca6dc73c37f8337cce6058411ec3665d0d31a
Kirellos Malak Habib <42212651+KiroMalakHabib@users.noreply.github.com>
2018-12-19 17:20:02 +0200

    again

--- src/eg/edu/alexu/csd/oop/game/cs01/SnapShot/SnapShot.java
+++ src/eg/edu/alexu/csd/oop/game/cs01/SnapShot/SnapShot.java
@@ -1,5 +1,41 @@
 package eg.edu.alexu.csd.oop.game.cs01.SnapShot;
 
-public class SnapShot {
+import java.beans.XMLDecoder;
+import java.beans.XMLEncoder;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+import eg.edu.alexu.csd.oop.game.World;
+
+public class SnapShot implements SnapShotInterface{
+
+	@Override
+	public void saveGame(World ourGame, String fileName) {
+		
+		try {
+			FileOutputStream fileStream = new FileOutputStream(new File("D:\\College\\CSED02\\OOP\\circusofplates\\" + fileName + ".xml"));
+			XMLEncoder encoder = new XMLEncoder(fileStream);
+			encoder.writeObject(ourGame);
+			encoder.close();
+			fileStream.close();
+		} catch (Exception e) {
+		}
+	}
+
+	@Override
+	public World loadGame(String path) {
+		try {
+			FileInputStream fileStream = new FileInputStream(new File(path));
+			XMLDecoder decoder = new XMLDecoder(fileStream);
+			World loadWorld = (World) decoder.readObject();
+			decoder.close();
+			fileStream.close();
+			return loadWorld;
+		} catch (Exception e) {
+			//chain of responsibility.
+			return null;
+		}
+	}
 	
 }
