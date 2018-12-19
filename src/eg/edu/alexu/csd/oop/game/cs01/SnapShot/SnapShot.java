f5729ff1362e411f32c246a7d106af800afb9545
Kirellos Malak Habib <42212651+KiroMalakHabib@users.noreply.github.com>
2018-12-19 17:19:39 +0200

    snapShot and begining of chain of responsibility.

---
+++ src/eg/edu/alexu/csd/oop/game/cs01/SnapShot/SnapShot.java
@@ -0,0 +1,5 @@
+package eg.edu.alexu.csd.oop.game.cs01.SnapShot;
+
+public class SnapShot {
+	
+}
---
+++ src/eg/edu/alexu/csd/oop/game/cs01/SnapShot/SnapShotInterface.java
@@ -0,0 +1,15 @@
+package eg.edu.alexu.csd.oop.game.cs01.SnapShot;
+
+import eg.edu.alexu.csd.oop.game.World;
+
+public interface SnapShotInterface {
+	/**
+	 * save game.
+	 * @param ourGame
+	 */
+	public void saveGame(World ourGame, String fileName);
+	/*
+	 * load game.
+	 */
+	public World loadGame(String path);
+}
